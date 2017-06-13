d56007ff6ea286b13b5de48e15fb4d428971862b
1amBulletproof <brandon.tarney@gmail.com>
2017-06-13 00:15:02 -0400

    make basic test of classes

--- Homework2_M4_605481/test/com/brandontarney/ContactTest.java
+++ Homework2_M4_605481/test/com/brandontarney/ContactTest.java
@@ -17,10 +17,96 @@
  */
 package com.brandontarney;
 
-/**
- *
- * @author Tarney
- */
+import java.util.Vector;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Assert;
+
 public class ContactTest {
-    
+
+    Vector<Destroyer> destroyers;
+    Vector<P3> p3s;
+    Vector<Submarine> subs;
+    Vector<Ship> ships;
+    Vector<Aircraft> aircraft;
+    Vector<Contact> contacts;
+
+    public ContactTest() {
+    }
+
+    @Before
+    public void setUp() {
+
+        destroyers = new Vector<Destroyer>();
+        p3s = new Vector<P3>();
+        subs = new Vector<Submarine>();
+        ships = new Vector<Ship>();
+        aircraft = new Vector<Aircraft>();
+        contacts = new Vector<Contact>();
+
+        Destroyer destroyer1 = new Destroyer(200, 20, "DDG_AWESOME", "DESTROYER", 5);
+        destroyers.add(destroyer1);
+        ships.add((Ship) destroyer1);
+        contacts.add((Contact) destroyer1);
+        Destroyer destroyer2 = new Destroyer(-100, -20, "", "", -5);
+        destroyers.add(destroyer2);
+        ships.add((Ship) destroyer2);
+        contacts.add(destroyer2);
+
+        Submarine sub1 = new Submarine(200, 10, "Silent But Deadly", "OHIO_CLASS", 2400);
+        subs.add(sub1);
+        ships.add((Ship) sub1);
+        contacts.add((Contact) sub1);
+        Submarine sub2 = new Submarine(100, 3000, "Fast and the Furious", "VIRGINIA_CLASS", 12);
+        subs.add(sub2);
+        ships.add((Ship) sub2);
+        contacts.add((Contact) sub2);
+
+        P3 p31 = new P3();
+        p31.setAltitude(-1);
+        p31.setLength(35);
+        p31.setName("EAGLE1");
+        p31.setNumberEngines(4);
+        p31.setSpeed("-1");
+        p31.setType("PLANE");
+        p3s.add(p31);
+        aircraft.add((Aircraft) p31);
+        contacts.add((Contact) p31);
+        P3 p32 = new P3(40, 101, 500, "HAWKEYE", "PLANE", 4);
+        p3s.add(p32);
+        aircraft.add((Aircraft) p32);
+        contacts.add((Contact) p32);
+
+    }
+
+    @Test
+    public void testEverything() {
+        System.out.println("\nTest Everything");
+
+        for (Contact contact : this.contacts) {
+            System.out.println(contact.toString());
+        }
+
+        Submarine subTest = new Submarine();
+        Assert.assertEquals(subTest.getName(), "no_name");
+        subTest.setName("SUBTACULAR");
+        Assert.assertEquals(subTest.getName(), "SUBTACULAR");
+
+        Assert.assertEquals(subTest.getNumberTorpedos(), 0);
+        subTest.setNumberTorpedos("1 billion");
+        Assert.assertEquals(subTest.getNumberTorpedos(), 0);
+        subTest.setNumberTorpedos("foo");
+        Assert.assertEquals(subTest.getNumberTorpedos(), 0);
+        subTest.setNumberTorpedos("-20");
+        Assert.assertEquals(subTest.getNumberTorpedos(), 0);
+        subTest.setNumberTorpedos("24");
+        Assert.assertEquals(subTest.getNumberTorpedos(), 24);
+        subTest.setNumberTorpedos(12);
+        Assert.assertEquals(subTest.getNumberTorpedos(), 12);
+
+    }
+
 }
